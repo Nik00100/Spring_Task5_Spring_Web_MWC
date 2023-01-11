21c02224c87bfe87c41616d7fa120cfe7d3cca34
kirillovnick <kirillovnick@mail.ru>
2023-01-11 11:58:58 +0300

    migration

--- src/main/java/ru/netology/model/Post.java
+++ src/main/java/ru/netology/model/Post.java
@@ -4,12 +4,18 @@ public class Post {
   private long id;
   private String content;
 
+  private transient boolean removed;
+
   public Post() {
   }
 
   public Post(long id, String content) {
     this.id = id;
     this.content = content;
+  }
+
+  public boolean isRemoved() {
+    return removed;
   }
 
   public long getId() {
@@ -27,4 +33,9 @@ public class Post {
   public void setContent(String content) {
     this.content = content;
   }
+
+  public void setRemoved(boolean removed) {
+    this.removed = removed;
+  }
+
 }
